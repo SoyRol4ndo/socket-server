a88fa8027e7da2f6952383fa2c141c7e43ae3e05
SoyAlejoDev <diazperezalejandro26@gmail.com>
2024-09-30 14:28:50 -0400

    FIX: create menu

--- src/models/sockets.ts
+++ src/models/sockets.ts
@@ -63,97 +63,44 @@ export class Sockets {
         // Emitir los datos recuperados al cliente conectado
         socket.emit("emit-data", formattedData);
 
-        // socket.on("send-item", async (data) => {
-        //   console.log(`Received send-item event with data: ${data}`);
-        //   const itemId = data[2][0].items[0].id;
-        //   const menuCategoryId = data[2][0].id;
-        //   await this.prisma.menuItem.create({
-        //     data: {
-        //       id: itemId,
-        //       nombre: data[2][0].items[0].nombre,
-        //       descripcion: data[2][0].items[0].descripcion,
-        //       precio: data[2][0].items[0].precio,
-        //       foto: data[2][0].items[0].foto,
-        //       menuCategoryId: menuCategoryId,
-        //     },
-        //   });
-        // });
-        socket.on("send-item", async (data) => {
+        interface MenuItem {
+          nombre: string;
+          descripcion: string;
+          precio: number;
+          foto: string;
+          habilitado: boolean;
+        }
+        socket.on("send-item", async (newMenu) => {
           try {
-            const newMenuCategory = await this.prisma.menuCategory.create({
+            // Destructuring del objeto recibido
+            const [{ nombre, items }] = newMenu;
+
+            console.log(`Creando nueva categoría: ${nombre}`);
+
+            // Creación de la categoría y sus ítems
+            await this.prisma.menuCategory.create({
               data: {
-                nombre: data.nombre,
+                nombre,
+                businessId: 1,
                 items: {
-                  create: data.items.map(
-                    (item: {
-                      nombre: string;
-                      descripcion: string;
-                      precio: number;
-                      foto: string;
-                      habilitado: boolean;
-                    }) => ({
-                      nombre: item.nombre,
-                      descripcion: item.descripcion,
-                      precio: item.precio,
-                      foto: item.foto,
-                      habilitado: item.habilitado,
-                    })
-                  ),
+                  create: items.map((item: MenuItem) => ({
+                    nombre: item.nombre,
+                    descripcion: item.descripcion,
+                    precio: item.precio,
+                    foto: item.foto,
+                    habilitado: item.habilitado,
+                  })),
                 },
-                businessId: data.businessId,
               },
             });
-            console.log(`New menu category created: ${newMenuCategory.nombre}`);
-            socket.emit("item-created", newMenuCategory);
+
+            console.log("Categoría y sus ítems creados con éxito");
           } catch (error) {
-            console.error(error);
-            socket.emit("error", error);
+            console.error("Error al crear la categoría:", error);
           }
         });
-        socket.on("page-data", async (data) => {
-          try {
-            const businessId = data.businessId;
-            const updates = data.updates;
-            const updatedBusiness = await this.prisma.business.update({
-              where: { id: businessId },
-              data: updates,
-            });
-            console.log(`Business updated: ${updatedBusiness.nombreNegocio}`);
-            socket.emit("business-updated", updatedBusiness);
-          } catch (error) {
-            console.error(error);
-            socket.emit("error", error);
-          }
-        });
-        // // Escuchar y manejar la orden guardada
-        // socket.on("saveOrder", data => {
-        // 	const orderTable = JSON.stringify(data);
-        // 	console.log(orderTable);
 
-        // 	// Emitir la confirmación de la orden guardada
-        // 	socket.emit("orderSaved", orderTable);
-        // });
-
-        // // Escuchar cuando la orden esté lista
-        // socket.on("orden-lista", data => {
-        // 	console.log(data);
-        // });
-
-        // // Escuchar cambios en el estado de las mesas
-        // socket.on("cambiar-estado-mesa", async ({id, estado}) => {
-        // 	try {
-        // 		await this.prisma.mesa.update({
-        // 			where: {id},
-        // 			data: {bussy: estado},
-        // 		});
-
-        // 		const mesasActualizadas = await this.prisma.mesa.findMany();
-        // 		this.io.emit("mesas-actualizadas", mesasActualizadas);
-        // 	} catch (error) {
-        // 		console.error("Error al actualizar el estado de la mesa:", error);
-        // 		socket.emit("error", "Error al actualizar el estado de la mesa");
-        // 	}
-        // });
+        socket.on("page-data", async (data) => {});
       } catch (error) {
         console.error("Error al recuperar los datos:", error);
         socket.emit("error", "Error al recuperar los datos");
